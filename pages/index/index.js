6b1175fc726a861c144f0957e585e08f83346339
高奕新 <13113010320@163.com>
2020-04-15 23:11:36 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,34 @@
 //Page Object
+import { request } from "../../request/index.js";
 Page({
   data: {
-    
+      // 轮播图数组
+      swiperList: [], 
   },
   //options(Object)
   onLoad: function(options) {
-    
+      // wx.request({
+      //   url:  'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      //   data: {},
+      //   header: {'content-type':'application/json'},
+      //   method: 'GET',
+      //   dataType: 'json',
+      //   responseType: 'text',
+      //   success: (result) => { 
+      //    this.setData({
+      //     swiperList: result.data.message
+      //   })
+      //   },
+      //   fail: () => {},
+      //   complete: () => {}
+      // });
+      request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'}).then((result)=>{
+        console.log(result);
+        
+              this.setData({
+          swiperList: result.data.message
+        })
+      })   
   },
   onReady: function() {
     
