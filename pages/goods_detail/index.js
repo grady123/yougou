b8f75d5067b61f7686d6b208d264b43714d303b2
高奕新 <13113010320@163.com>
2020-04-18 14:35:06 +0800

    完成详情页面

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -10,59 +10,48 @@ Page({
     // 商品是否被收藏
     isCollect:false
   },
-  /**
+   // 商品对象
+   GoodsInfo: {},
+ /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    let pages = getCurrentPages();
+    let currentPage = pages[pages.length - 1];
+    let options = currentPage.options;
+    const { goods_id } = options;
+    this.getGoodsDetail(goods_id);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+   // 获取商品详情数据
+   async getGoodsDetail(goods_id) {
+    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    this.GoodsInfo = goodsObj;
+    // 1 获取缓存中的商品收藏的数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 2 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iphone部分手机 不识别 webp图片格式 
+        // 最好找到后台 让他进行修改 
+        // 临时自己改 确保后台存在 1.webp => 1.jpg 
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      },
+      isCollect
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+   // 点击轮播图 放大预览
+   handlePrevewImage(e) {
+    // 1 先构造要预览的图片数组 
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    // 2 接收传递过来的图片url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
