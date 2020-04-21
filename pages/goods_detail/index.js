b34f641e9ee9ddc468c63647f5b49154972985d7
高奕新 <13113010320@163.com>
2020-04-21 09:32:46 +0800

    添加购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -43,6 +43,67 @@ Page({
       isCollect
     })
   },
+    // 点击 加入购物车
+    handleCartAdd() {
+      // 1 获取缓存中的购物车 数组
+      let cart = wx.getStorageSync("cart") || [];
+      // 2 判断 商品对象是否存在于购物车数组中
+      let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+      if (index === -1) {
+        //3  不存在 第一次添加
+        this.GoodsInfo.num = 1;
+        this.GoodsInfo.checked = true;
+        cart.push(this.GoodsInfo);
+      } else {
+        // 4 已经存在购物车数据 执行 num++
+        cart[index].num++;
+      }
+      // 5 把购物车重新添加回缓存中
+      wx.setStorageSync("cart", cart);
+      // 6 弹窗提示
+      wx.showToast({
+        title: '加入成功',
+        icon: 'success',
+        // true 防止用户 手抖 疯狂点击按钮 
+        mask: true
+      });
+    },
+      // 点击 商品收藏图标
+  handleCollect(){
+    let isCollect=false;
+    // 1 获取缓存中的商品收藏数组
+    let collect=wx.getStorageSync("collect")||[];
+    // 2 判断该商品是否被收藏过
+    let index=collect.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    // 3 当index！=-1表示 已经收藏过 
+    if(index!==-1){
+      // 能找到 已经收藏过了  在数组中删除该商品
+      collect.splice(index,1);
+      isCollect=false;
+      wx.showToast({
+        title: '取消成功',
+        icon: 'success',
+        mask: true
+      });
+        
+    }else{
+      // 没有收藏过
+      collect.push(this.GoodsInfo);
+      isCollect=true;
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        mask: true
+      });
+    }
+    // 4 把数组存入到缓存中
+    wx.setStorageSync("collect", collect);
+    // 5 修改data中的属性  isCollect
+    this.setData({
+      isCollect
+    }) 
+  },
+
    // 点击轮播图 放大预览
    handlePrevewImage(e) {
     // 1 先构造要预览的图片数组 
