bd3702eff4d02af21dd5a1ca22b0677532d5ed9a
高奕新 <13113010320@163.com>
2020-04-22 17:39:44 +0800

    完成搜索和支付

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,45 @@
-// pages/order/index.js
+
+/* 
+1 页面被打开的时候 onShow 
+  0 onShow 不同于onLoad 无法在形参上接收 options参数 
+  0.5 判断缓存中有没有token 
+    1 没有 直接跳转到授权页面
+    2 有 直接往下进行 
+  1 获取url上的参数type
+  2 根据type来决定页面标题的数组元素 哪个被激活选中 
+  2 根据type 去发送请求获取订单数据
+  3 渲染页面
+2 点击不同的标题 重新发送请求来获取和渲染数据 
+ */
+
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    orders: [],
+    tabs: [
+      {
+        id: 0,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待发货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/pay/index.js
+++ pages/pay/index.js
@@ -65,6 +65,7 @@ Page({
         await requestPayment(pay);
         // 7 查询后台 订单状态
         const res = await request({ url: "/my/orders/chkOrder", method: "POST", data: { order_number } });
+ 
         await showToast({ title: "支付成功" });
         // 8 手动删除缓存中 已经支付了的商品
         let newCart=wx.getStorageSync("cart");
