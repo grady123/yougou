091832cc0f38679be60707252394d87b7d974475
高奕新 <13113010320@163.com>
2020-04-23 23:30:45 +0800

    完成收藏页面

--- utils/asyncWx.js
+++ utils/asyncWx.js
@@ -79,3 +79,22 @@ export const login=()=>{
   })
 }
 
+/**
+ * promise 形式的 小程序的微信支付
+ * @param {object} pay 支付所必要的参数
+ */
+export const requestPayment=(pay)=>{
+  return new Promise((resolve,reject)=>{
+   wx.requestPayment({
+      ...pay,
+     success: (result) => {
+      resolve(result)
+     },
+     fail: (err) => {
+       reject(err);
+     }
+   });
+     
+  })
+}
+
