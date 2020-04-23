22bb790141c55180f725792231418d8194a777f7
高奕新 <13113010320@163.com>
2020-04-24 00:37:29 +0800

    完成订单页面

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -11,6 +11,8 @@ Page({
   const { encryptedData, rawData, iv, signature } = e.detail;
   // 2 获取小程序登录成功后的code
   const { code } = await login();
+  console.log(code)
+  console.log(1111111111)
   const loginParams={ encryptedData, rawData, iv, signature ,code};
   //  3 发送请求 获取用户的token
   // const {token}=await request({url:"/users/wxlogin",data:loginParams,method:"post"});
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,4 +1,3 @@
-
 /* 
 1 页面被打开的时候 onShow 
   0 onShow 不同于onLoad 无法在形参上接收 options参数 
@@ -16,6 +15,10 @@ import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
     orders: [],
     tabs: [
@@ -42,4 +45,49 @@ Page({
     ]
   },
 
+  onShow(options) {
+    const token = wx.getStorageSync("token");
+    if (!token) {
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return;
+    }
+
+
+
+    // 1 获取当前的小程序的页面栈-数组 长度最大是10页面 
+    let pages = getCurrentPages();
+    // 2 数组中 索引最大的页面就是当前页面
+    let currentPage = pages[pages.length - 1];
+    // 3 获取url上的type参数
+    const { type } = currentPage.options;
+    // 4 激活选中页面标题 当 type=1 index=0 
+    this.changeTitleByIndex(type-1);
+    this.getOrders(type);
+  },
+  // 获取订单列表的方法
+  async getOrders(type) {
+    const res = await request({ url: "/my/orders/all", data: { type } });
+    this.setData({
+      orders: res.orders.map(v=>({...v,create_time_cn:(new Date(v.create_time*1000).toLocaleString())}))
+    })
+  },
+  // 根据标题索引来激活选中 标题数组
+  changeTitleByIndex(index) {
+    // 2 修改源数组
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3 赋值到data中
+    this.setData({
+      tabs
+    })
+  },
+  handleTabsItemChange(e) {
+    // 1 获取被点击的标题索引
+    const { index } = e.detail;
+    this.changeTitleByIndex(index);
+    // 2 重新发送请求 type=1 index=0
+    this.getOrders(index+1);
+  }
 })
